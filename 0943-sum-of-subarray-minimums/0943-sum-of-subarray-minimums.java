da333376e170d20e390554451cced2f868dcec0b
ramkumar0502 <141810536+ramkumar0502@users.noreply.github.com>
2025-09-16 00:23:28 +0530

    Time: 64 ms (63.15%), Space: 52.7 MB (21.99%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -1,51 +1,35 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
+        int nse[]=new int[arr.length];
+        int pse[]=new int[arr.length];
         Stack<Integer> st=new Stack<>();
-        int ple[]=new int[arr.length];
-        int nle[]=new int[arr.length];
-   
-        int MOD = 1000000007;
+        int mod=1000000007;
         for(int i=0;i<arr.length;i++)
         {
-            while((!st.isEmpty()) &&  arr[i]<arr[st.peek()])
+            while((!st.isEmpty())&&arr[st.peek()]>arr[i])
             {
                 st.pop();
             }
-            if(!st.isEmpty() )
-            {
-                ple[i]=i-st.peek();
-            }
-           else
-            {
-                ple[i]=i+1;
-            }
+            pse[i]=st.isEmpty()?i+1:i-st.peek();
             st.push(i);
         }
         st.clear();
         for(int i=arr.length-1;i>=0;i--)
         {
-            while((!st.isEmpty())&& arr[i]<=arr[st.peek()])
+            while((!st.isEmpty())&& arr[st.peek()]>=arr[i])
             {
                 st.pop();
             }
-            if(!st.isEmpty())
-            {
-                nle[i]=st.peek()-i;
-            }
-            else
-            {
-                nle[i]=arr.length-i;
-            }
+            nse[i]=st.isEmpty()?arr.length-i:st.peek()-i;
             st.push(i);
         }
-
-        long total = 0;
-        for (int i = 0; i < arr.length; i++) {
-            long prod = ((long) arr[i] * ple[i] * nle[i]) % MOD;
-            total = (total + prod) % MOD;
+          long ans = 0;
+        for (int i = 0; i <arr.length; i++) {
+            long contribution = (long) arr[i] * pse[i] * nse[i];
+            ans = (ans + contribution) % mod;
         }
 
-        return (int) total;
+        return (int) ans;
         
     }
 }
