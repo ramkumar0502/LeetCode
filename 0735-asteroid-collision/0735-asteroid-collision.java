5d221b09b8cf0a60ac408eb08446d8737a61d45d
ramkumar0502 <141810536+ramkumar0502@users.noreply.github.com>
2025-09-18 18:56:56 +0530

    Time: 9 ms (8.18%), Space: 45.4 MB (73.67%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,47 +1,31 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st=new Stack<>();
-        for(int i=0;i<asteroids.length;i++)
-        {
-            if( (!st.isEmpty()) && st.peek()>0 && asteroids[i]<0)
-            {
-                while((!st.isEmpty()) && st.peek()>0 && Math.abs(asteroids[i])>st.peek())
-                {
-                    st.pop();
-                }
-                if((!st.isEmpty())&& st.peek()>0 && asteroids[i]<0)
-                {
-                   if(st.peek()==Math.abs(asteroids[i]))
-                   {
-                   st.pop();
-                   continue;
-                   }
-                   if(st.peek()>Math.abs(asteroids[i]))
-                   {
-                      continue;
-                   }
-                }
-               
-                if (st.isEmpty() || st.peek() < 0) {
-                    st.push(asteroids[i]);
-                }
-            }
-           
-            else
-            {
-                st.push(asteroids[i]);
-
-            }
-        }
+       Stack<Integer>st=new Stack<>();
+       for(int i=0;i<asteroids.length;i++)
+       {
+         while((!st.isEmpty())&&st.peek()>0&&asteroids[i]<0&&Math.abs(asteroids[i])>st.peek())
+         {
+            st.pop();
+         }
+         if((!st.isEmpty())&&st.peek()>0 && asteroids[i]<0 && Math.abs(asteroids[i])<st.peek() )
+         {
+            continue;
+         }
+         else if((!st.isEmpty())&&st.peek()>0 && asteroids[i]<0&& Math.abs(asteroids[i])==st.peek() )
+         {
+            st.pop();
+            continue;
+         }
+         else
+         {
+            st.push(asteroids[i]);
+         }
+       }
         int result[]=new int[st.size()];
         for(int i=result.length-1;i>=0;i--)
         {
-            result[i]=st.peek();
-            st.pop();
+            result[i]=st.pop();
         }
         return result;
-
-
-        
     }
 }
