e30e80c3c98390b2195e30380668c1bd0bd0c033
ramkumar0502 <141810536+ramkumar0502@users.noreply.github.com>
2025-09-19 09:09:26 +0530

    Time: 63 ms (79.02%), Space: 57.5 MB (53.72%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,43 +1,37 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> st=new Stack<>();
-        int maxarea=Integer.MIN_VALUE;
-        int cal=0;
-        for(int i=0;i<heights.length;i++)
+    Stack<Integer> st=new Stack<>();
+    int max=Integer.MIN_VALUE;
+    int cal=0;
+    for(int i=0;i<heights.length;i++)
+    {
+        while((!st.isEmpty())&&heights[st.peek()]>heights[i])
         {
-            
-            while(! st.isEmpty() &&heights[st.peek()]>heights[i])
+            int val=heights[st.pop()];
+            if(st.isEmpty())
             {
-                int index=st.pop();
-                if(st.isEmpty())
-                {
-                    cal=heights[index]*(i-(-1)-1);
-                }
-                else
-                {
-                    cal=heights[index]*(i-st.peek()-1);
-                }
-                maxarea=Math.max(cal,maxarea);
-               
+                max=Math.max(max,val*(i-(-1)-1));
             }
-            st.push(i);
-            
+            else
+            {
+            max=Math.max(max,val*(i-st.peek()-1));
+            }
         }
-        while(!st.isEmpty())
-        {
-            int n=heights.length;
-            int current=st.pop();
-             if(st.isEmpty())
-                {
-                    cal=heights[current]*(n-(-1)-1);
-                }
-                else
-                {
-                    cal=heights[current]*(n-st.peek()-1);
-                }
-            maxarea=Math.max(cal,maxarea);
-        }
-        return maxarea;
-        
+        st.push(i);
     }
+    while(!st.isEmpty())
+    {
+       int n=heights.length;
+       int val=heights[st.pop()];
+       if(st.isEmpty())
+       {
+         max=Math.max(max,val*(n-(-1)-1));
+       }
+       else
+       {
+        max=Math.max(max,val*(n-(st.peek())-1));
+       }
+    }
+    return max;
+}
 }
