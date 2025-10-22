8cfa5f39dae0eac3d70c3339aaaaecb2cac6aa71
ramkumar0502 <141810536+ramkumar0502@users.noreply.github.com>
2025-10-22 20:51:00 +0530

    Time: 2 ms (84.58%), Space: 44.8 MB (46.51%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       List<List<Integer>> result=new ArrayList<>();
-       List<Integer>li=new ArrayList<>();
-       int sum=0;
-       int index=0;
-       int n=candidates.length;
-       value(candidates,target,sum,result,li,index,n);
-       return result;
+        List<List<Integer>>result=new ArrayList<>();
+        List<Integer>temp=new ArrayList<>();
+        int start=0;  
+        recursion(candidates,target,start,result,temp);
+        return result;
+}
 
-    }
-    public static void value(int candidates[],int target,int sum,List<List<Integer>> result,List<Integer>li,int index,int n)
+public void recursion (int candidates[],int target,int start,List<List<Integer>> result,List<Integer> temp)
+{
+    if(target==0)
     {
-        if(target==0)
-        {
-            result.add(new ArrayList<>(li));
-            return;
-        }
-        if(index==n||target<0)
-        {
-            return;
-        }
-        li.add(candidates[index]);
-        value(candidates,target-candidates[index],sum+candidates[index],result,li,index,n);
-        li.remove(li.size()-1);
-        value(candidates ,target,sum,result,li,index+1,n);
-
+        result.add(new ArrayList<>(temp));
+        return;
     }
+    if(target<0||start==candidates.length)
+    {
+        return;
+    }
+    temp.add(candidates[start]);
+    recursion(candidates,target-candidates[start],start,result,temp);
+    temp.remove(temp.size()-1);
+    recursion(candidates,target,start+1,result,temp);
+}
 }
